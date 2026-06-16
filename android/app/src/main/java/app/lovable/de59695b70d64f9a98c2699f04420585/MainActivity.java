b8a2d6ac15bd208dea9bcfd88b6ae008c9cbec93
KejaSure <kejasureke@gmail.com>
2026-06-16 12:55:54 -0400

    Added mobile folders

---
+++ android/app/src/main/java/app/lovable/de59695b70d64f9a98c2699f04420585/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.de59695b70d64f9a98c2699f04420585;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.de59695b70d64f9a98c2699f04420585',
-  appName: 'KejaSure',
+  appId: 'com.kejasure.rentalapp',
+  appName: 'Kejasure',
   webDir: 'dist',
   server: {
-    url: 'https://de59695b-70d6-4f9a-98c2-699f04420585.lovableproject.com?forceHideBadge=true',
+    url: 'https://kejasure.pages.dev',
     cleartext: true
   }
 };
